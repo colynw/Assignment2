a9fbbed2c4767a387b64e59da12fe47c438bebd5
Jan Zvěřina <zverija4@fit.cvut.cz>
2019-03-27 22:18:11 -0500

    AddInterest method and few changes
    Implemented addInterest method.
    Variables are now protected, not private.
    Refactor of debit and credit methods to withdraw and deposit methods.

--- src/Account.java
+++ src/Account.java
@@ -1,74 +1,72 @@
 import javax.swing.JOptionPane;
 
 public class Account {
-	private int id;
-	private String strName;
-	private char accountType;
-	private double balance;
-	
+	protected int id;
+	protected String strName;
+	protected char accountType;
+	protected double balance;
+
 	public void setID(int id) {
 		this.id = id;
 	}
-	
+
 	public int getID() {
 		return this.id;
 	}
-	
+
 	public void setName(String name) {
 		this.strName = name;
 	}
-	
+
 	public String getName() {
 		return this.strName;
 	}
-	
+
 	public void setAccountType(char type) {
 		this.accountType = type;
 	}
-	
+
 	public char getAccountType() {
 		return this.accountType;
 	}
-	
+
 	public void setBalance(double blance) {
 		this.balance = blance;
 	}
-	
+
 	public double getBalance() {
 		return this.balance;
 	}
-	
+
 	Account() {
 		this.strName = "TTU1";
 		this.accountType = 'C';
 		this.balance = 0.0;
 	}
-	
+
 	Account(int id, String name, char type, double balance) {
 		this.id = id;
 		this.strName = name;
 		this.accountType = type;
 		this.balance = balance;
 	}
-	
-	//withdraw
-	public void debit(double amount) {
+
+	public void withdraw(double amount) {
 		if(balance >= amount) {
 			balance-=amount;
 		}else {
 			JOptionPane.showMessageDialog(null, "Insufficient balance.");
 		}
 	}
-	
-	//deposit
-	public void credit(double amount) {
+
+	public void deposit(double amount) {
 		balance+=amount;
 	}
-	
+
 	public void displayBalance() {
 		JOptionPane.showMessageDialog(null, "The balance is "+balance);
 	}
-	
+
 	public static void displayAll(Account[] arr) {
 		String str = "";
 		for (int i=0;i<arr.length;i++) {
@@ -76,4 +74,4 @@ public class Account {
 		}
 		JOptionPane.showMessageDialog(null, str);
 	}
- }
+}
