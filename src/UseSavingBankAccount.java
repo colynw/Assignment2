6d7e6558bea6cbf24e3e8d9f2016f46b9d389893
colynw <colyndimas@rocketmail.com>
2019-04-01 18:55:45 -0500

    Update UseSavingBankAccount.java

--- src/UseSavingBankAccount.java
+++ src/UseSavingBankAccount.java
@@ -1,17 +1,16 @@
-
 public class UseSavingBankAccount{
-	public static SavingAccount account_arr[];
-	
-	public static void initAccounts() {
-		account_arr = new SavingAccount[50];
-		for (int i=0;i<50;i++) {
-			account_arr[i] = new SavingAccount((i+1),"TTU"+(i+1),'C',0.0,5.0);
-		}
-	}
+    public static SavingAccount account_arr[];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		initAccounts();
-		GUI gui = new GUI();
-	}
+    public static void initAccounts() {
+        account_arr = new SavingAccount[50];
+        for (int i=0;i<50;i++) {
+            account_arr[i] = new SavingAccount((i+1),"TTU"+(i+1),'C',0.0,5.0, 0.0 );
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        initAccounts();
+        GUI gui = new GUI();
+    }
 }
